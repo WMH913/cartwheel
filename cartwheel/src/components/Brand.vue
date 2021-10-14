c0ee96b41038ee7ea240626e419a9bd58e3e8a9f
zhangzizhen <2692977784@qq.com>
2021-10-14 14:58:09 +0800

    车辆展示

--- cartwheel/src/components/Brand.vue
+++ cartwheel/src/components/Brand.vue
@@ -1,5 +1,39 @@
 <template>
+  <div>
     <div>
-        真真的2
+      <mt-cell-swipe
+        v-for="(CarProduct, index) in CarProducts"
+        :key="index"
+        :title="CarProduct.GroupName"
+      >
+      </mt-cell-swipe>
     </div>
+  </div>
 </template>
+<script>
+import { Cell, CellSwipe } from "mint-ui";
+export default {
+  components: {
+    Cell,
+    CellSwipe
+  },
+  data() {
+    return {
+      CarProducts: null
+    };
+  },
+  created() {
+    var app = this;
+    this.$axios
+      .get(
+        "https://baojia.chelun.com/v2-car-getMakeListByMasterBrandId.html?MasterID=2"
+      )
+      .then(function(res) {
+        console.log(res.data.data);
+        app.CarProducts = res.data.data;
+      });
+  },
+  methods: {}
+};
+</script>
+<style scoped></style>
